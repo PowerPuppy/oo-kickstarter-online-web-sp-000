edd770a53e990c777b7841ca379e71da644ad938
Chris Cheong <chrisxcheong@gmail.com>
2019-08-01 18:18:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,4 +2,17 @@ class Backer
 attr_accessor :name
 @@all = []
 
+def initialize(name)
+  @name = name
+  @projects = []
+end
+
+def backed_projects
+  @projects
+end
+
+def self.all
+  @@all
+end
+
 end
