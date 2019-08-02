d60865faa7796bbd4d726fd2a49faa800daeac3e
Chris Cheong <chrisxcheong@gmail.com>
2019-08-02 00:06:13 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,6 +12,10 @@ def back_project(project)
   self.backed_projects << project
   project.backer << self
 end
+def add_backer(backer)
+  self.backers << backer
+  backer.backed_projects << self
+end
 
 def self.all
   @@all
