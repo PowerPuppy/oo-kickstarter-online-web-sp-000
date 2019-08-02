f25880a3bde8ff0917cbcd5737808c3e6b96e81a
Chris Cheong <chrisxcheong@gmail.com>
2019-08-02 00:01:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,24 +1,11 @@
 class Project
-attr_accessor :title
-attr_reader :backers
+attr_accessor :backers
+attr_reader :title
 @@all = []
 
-def initalize(title)
+def initialize(title)
   @title = title
   @backers = []
-end
-
-def add_backer(backer)
-  self.backers << backer
-  backer.backed_projects << self
-end
-
-def self.all
-  @@all
-end
-
-def backers
-  @backers
 end
 
 end
